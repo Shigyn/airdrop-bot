fa790cfb867959c8e6ce29b715f912ad48ad6082
Shigyn <masia.nicolas.07@gmail.com>
2025-05-16 23:31:04 +0200

    Upgrade

--- logger.js
+++ logger.js
@@ -1,15 +1,4 @@
-const winston = require('winston');
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`)
-  ),
-  transports: [
-    new winston.transports.Console()
-  ],
-});
-
-module.exports = logger;
+module.exports = {
+  error: (...args) => console.error(...args),
+  info: (...args) => console.log(...args),
+};
