6ba8fff69e63c87873ecb6d9ce041ae9e0fd0db6
Shigyn <masia.nicolas.07@gmail.com>
2025-05-16 18:59:28 +0200

    Remove winston completely

--- logger.js
+++ logger.js
@@ -1,4 +1,4 @@
-// src/logger.js
+// src/logger.js - Version simplifiée SANS Winston
 module.exports = {
   info: (...args) => console.log(`[INFO] ${new Date().toISOString()}`, ...args),
   error: (...args) => console.error(`[ERROR] ${new Date().toISOString()}`, ...args),
